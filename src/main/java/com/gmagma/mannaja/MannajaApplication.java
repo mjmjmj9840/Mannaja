21335208a81c04ad24437a34267a9b9c944b6a53
kmj <mjmjmj9840@gmail.com>
2021-07-29 15:01:08 +0900

    before eject

---
+++ src/main/java/com/gmagma/mannaja/MannajaApplication.java
@@ -0,0 +1,13 @@
+package com.gmagma.mannaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannajaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MannajaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmagma/mannaja/MannajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmagma.mannaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MannajaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
