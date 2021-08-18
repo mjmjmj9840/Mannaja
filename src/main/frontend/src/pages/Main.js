a9c508fd6902af5c2d8d85959eb6ebe64981d4ec
kjk <jinkyung1016@gmail.com>
2021-08-19 01:21:37 +0900

    1. 메인화면 진행중

--- src/main/frontend/src/pages/Main.js
+++ src/main/frontend/src/pages/Main.js
@@ -2,25 +2,72 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div id="group_main_wrap" className="flex text_white">
+    <div id="group_main_wrap" className="flex text_gray">
       <div id="member_notice_wrap" className="items flex flex_column">
         <div id="member_wrap">
-          <p className="text_20px text_bold">안양 돼지파티</p>
-          <p className="text_boldM">&lt;그룹원&gt;</p>
-          {/* <그룹원> 으로 표기하면 그룹원 태그로 인식됨 */}
-          <p className="group_member text_bold group_reader">
-            불주먹에이스<span className="text_13px"> (그룹장) </span>
-          </p>
-          <p className="group_member">냠냠쩝쩝</p>
-          <p className="group_member">일이삼사오육칠팔구십</p>
-          <p className="group_member">돼지1</p>
-          <p className="group_member">돼지2</p>
-          <p className="group_member">돼지3</p>
-          <p className="group_member">돼지4</p>
-          <p className="group_member">돼지5</p>
-          <p className="group_member">돼지6</p>
+          <div id="group_name" className="text_center text_white">
+            <p className="text_20px text_bold">일이삼사오육칠팔구십</p>
+          </div>
+          <div id="group_member">
+            {/* <그룹원> 으로 표기하면 그룹원 태그로 인식됨 */}
+            <p className="group_leader">
+              그룹장닉네임
+              <img src="/images/leader_crown.png" alt="그룹장 표시" />
+            </p>
+            <p className="text_bold">일이삼사오육칠팔구십</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+            <p>닉네임</p>
+          </div>
         </div>
-        <div id="notice_wrap"></div>
+        <div>
+          <div id="notice_area" className="text_center text_white">
+            <p className="text_20px text_bold">Notice</p>
+          </div>
+          <div className="notice_wrap">
+            <div className="notice text_center">
+              <p className="text_bold">
+                일이삼사오육칠팔구십<span> 2021.08.19 00:00:00 </span>
+              </p>
+              <p>
+                일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
+              </p>
+            </div>
+            <div className="notice text_center">
+              <p className="text_bold">
+                닉네임<span> yyyy.mm.dd hh:mi:ss </span>
+              </p>
+              <p>공지 제목</p>
+            </div>
+            <div className="notice text_center">
+              <p className="text_bold">
+                닉네임<span> yyyy.mm.dd hh:mi:ss </span>
+              </p>
+              <p>공지 제목</p>
+            </div>
+            <div className="notice text_center">
+              <p className="text_bold">
+                닉네임<span> yyyy.mm.dd hh:mi:ss </span>
+              </p>
+              <p>공지 제목</p>
+            </div>
+            <div className="notice text_center">
+              <p className="text_bold">
+                닉네임<span> yyyy.mm.dd hh:mi:ss </span>
+              </p>
+              <p>공지 제목</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="items flex">
         <div id="meeting_wrap"></div>
